a9db9eeaa15f9a84a2013d8dc4b40455329c4938
MishailEx <Mishail.avram@mail.ru>
2021-07-21 19:27:24 +0300

    2. Сокобан. Тестовое задание. [#75283 #180163]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,52 +2,40 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        rsl = checkHorizontal(board) || checkVertical(board);
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                rsl = true;
+                break;
+            }
+        }
         return rsl;
     }
 
-    public static boolean checkVertical(int[][] board) {
-        boolean rsl = false;
-        int column = 0;
-        int counter = 0;
-        while (column < board.length) {
-            for (int i = 0; i < board.length; i++) {
-                if (counter == board.length) {
-                    i = board.length - 1;
-                    rsl = true;
-                    break;
-                }
-                if (board[i][column] == 0) {
-                    break;
-                } else if (board[i][column] != 0) {
-                    counter++;
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i] == board[row]) {
+                for (int j = 0; j < board.length; j++) {
+                    if (board[i][j] != 1) {
+                        result = false;
+                        break;
+                    }
                 }
             }
-            column++;
         }
-        return rsl;
+        return result;
     }
 
-    public static boolean checkHorizontal(int[][] board) {
-        boolean rsl = false;
-        int counter = 0;
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board.length; j++) {
-                    if (counter == board.length){
-                        rsl = true;
-                        i = board.length - 1;
-                        break;
-                    }
-                    if (board[i][j] == 0){
-                        counter = 0;
-                        break;
-                    }else if (board[i][j] != 0){
-                        counter++;
-                    }
-                }
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
+            }
         }
-        return rsl;
+        return result;
     }
 }
 
